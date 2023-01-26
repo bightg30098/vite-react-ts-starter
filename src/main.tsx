14b4ce59fd63da635609a27c3a6fd164c6d5dae9
bightg30098 <bightg30098@gmail.com>
2023-01-26 23:45:10 +0800

    feat: setup msw

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,20 @@ import App from './App'
 
 import '@/styles/index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+async function prepare() {
+  if (process.env.NODE_ENV === 'development') {
+    const { worker } = await import('./__mocks__/browser')
+
+    return worker.start()
+  }
+
+  return Promise.resolve()
+}
+
+prepare().then(() =>
+  ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  ),
 )
